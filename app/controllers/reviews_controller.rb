73772705d0c60a497f000e4e07f2234c5f0f0e55
Teemu Frisk <teemu.frisk@gmail.com>
2014-10-22 22:04:20 +0300

    Basic review tagging functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,6 @@ class ReviewsController < ApplicationController
 
   private
     def review_params
-      params.require(:review).permit(:reviewer, :text)
+      params.require(:review).permit(:reviewer, :text, :tag_list)
     end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+  acts_as_taggable
 end
