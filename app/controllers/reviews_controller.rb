408cf5196992a0774f4a4f6f81e6b1913f636537
Teemu Frisk <teemu.frisk@gmail.com>
2014-10-23 11:23:37 +0300

    User authorization, admin and others

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.create(review_params)
+    @review.update_attribute :user_id, current_user.id
     redirect_to product_path(@product)
   end
 
