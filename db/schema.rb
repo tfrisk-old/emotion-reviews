b0ad02a2da3aac0117bf134bb62e5fb5ac6c5470
Teemu Frisk <teemu.frisk@gmail.com>
2014-12-03 14:12:27 +0200

    Add company model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141023074312) do
+ActiveRecord::Schema.define(version: 20141203120741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "company_id"
   end
+
+  add_index "products", ["company_id"], name: "index_products_on_company_id", using: :btree
 
   create_table "reviews", force: true do |t|
     t.string   "reviewer"
