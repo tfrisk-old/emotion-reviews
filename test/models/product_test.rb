5e9d29bf8b8677c3c9784f508d93fa0c0a626837
Teemu Frisk <teemu.frisk@gmail.com>
2015-02-06 11:42:01 +0200

    Add very basic tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @company = companies(:apple)
+  end
+
+  test "should be valid" do
+    assert @company.valid?
+  end
+
+  test "name should be present" do
+    @company.name = nil
+    assert_not @company.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @admin_user = users(:admin)
+    @regular_user = users(:joe)
+  end
+
+  test "should be valid" do
+    assert @regular_user.valid?
+  end
+
+  test "email should be present" do
+    @regular_user.email = nil
+    assert_not @regular_user.valid?
+  end
 end
